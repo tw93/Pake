22f07d250f311ffa27cb0f56ee3b60011f32a628
Aiello Chan <aiello.chan@gmail.com>
2022-10-20 16:18:38 +0800

    feat(wolai): Support wolai.com

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -155,7 +155,7 @@ fn main() -> wry::Result<()> {
     };
 
     let _webview = WebViewBuilder::new(window)?
-        .with_url("https://weread.qq.com/")?
+        .with_url("https://www.wolai.com/")?
         // .with_devtools(true)
         .with_initialization_script(script)
         .with_ipc_handler(handler)
