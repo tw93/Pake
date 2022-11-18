084e931a7e33b9dd7ed05e8e4bb08d9ef9cf2477
Tlntin <TlntinDeng01@Gmail.com>
2022-11-18 20:44:13 +0800

    增加Linux中文包名支持,优化批量打包程序

--- build.sh
+++ build.sh
@@ -7,29 +7,37 @@ fi
 # 依次填入app名称，链接
 # Fill in the app name and link in turn
 app_list=(
-  "weRead  weread.qq.com"
-  "aliyuque  www.yuque.com"
-  "flomo  flomoapp.com"
-  "weRead  weread.qq.com"
+  "weread 微信阅读  weread.qq.com"
+  "aliyuque 语雀  www.yuque.com"
+  "flomo 浮墨 flomoapp.com"
+  "weread 微信阅读 weread.qq.com"
 )
 
-old_name="weRead"
+old_name="weread"
+old_zh_name="微信阅读"
 old_url="weread.qq.com"
 for app_info in "${app_list[@]}"; do
   array=($app_info)
   package_name=${array[0]}
-  url=${array[1]}
+  package_zh_name=${array[1]}
+  url=${array[2]}
   echo "update package name and url"
   # replace package info
   sed -i "s/${old_url}/${url}/g" src-tauri/tauri.conf.json
   sed -i "s/${old_name}/${package_name}/g" src-tauri/tauri.conf.json
   
-  # update package info
-  old_name=${package_name}
-  old_url=${url}
-
   echo "update ico with 32x32 pictue"
   cp "src-tauri/png/${package_name}_32.png" "src-tauri/icons/icon.ico"
+
+  echo "update desktop"
+  mv "src-tauri/assets/com-tw93-${old_name}.desktop" "src-tauri/assets/com-tw93-${package_name}.desktop"
+  sed -i "s/${old_zh_name}/${package_zh_name}/g" "src-tauri/assets/com-tw93-${package_name}.desktop"
+  sed -i "s/${old_name}/${package_name}/g" "src-tauri/assets/com-tw93-${package_name}.desktop"
+
+  # update package info
+  old_zh_name=${package_zh_name}
+  old_name=${package_name}
+  old_url=${url}
 
   echo "build deb/appImage package for Linux x86-64"
   echo "package name is ${package_name}"
