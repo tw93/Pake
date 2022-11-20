5818af0dedfb429cbdb5f89f59fa1385114417ed
Tlntin <TlntinDeng01@Gmail.com>
2022-11-20 10:35:21 +0800

    更新Linux批量打包脚本

--- script/build.sh
+++ script/build.sh
@@ -21,56 +21,57 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
       mkdir output/macos
   fi
 fi
-
-
-
-
-# total package number
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+# total app number, ignore first line
 export total=`sed -n '$=' app.csv`
+total=$(($total-1))
 export index=1
 
 old_name="weread"
+old_title="WeRead"
 old_zh_name="微信阅读"
-old_url="weread.qq.com"
+old_url="https://weread.qq.com/"
 package_prefix="com-tw93"
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
   echo "==============="
   echo "Build for Linux"
   echo "==============="
+  export sd=${SHELL_FOLDER}/sd-linux-x64
   # for linux, package name may be com.xxx.xxx
   echo "rename package name"
   export desktop_file="src-tauri/assets/${package_prefix}.weread.desktop"
-  sed -i "s/\"productName\": \"weread\"/\"productName\": \"${package_prefix}-weread\"/g" src-tauri/tauri.conf.json
+  # sed -i "s/\"productName\": \"weread\"/\"productName\": \"${package_prefix}-weread\"/g" src-tauri/tauri.conf.json
+  $sd "\"productName\": \"weread\"" "\"productName\": \"${package_prefix}-weread\"" src-tauri/tauri.conf.json
+
 fi
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
   echo "==============="
   echo "Build for MacOS"
   echo "==============="
+
+  export sd=${SHELL_FOLDER}/sd-apple-x64
+  echo "rename package name"
+  $sd "\"productName\": \"weread\"" "\"productName\": \"WeRead\"" src-tauri/tauri.conf.json
 fi
 
-while read line
+tail -n +2 app.csv | while IFS=, read -r -a arr;
 do
-  package_name=$(echo ${line} | cut -d , -f 1)
-  package_zh_name=$(echo ${line} | cut -d , -f 2)
-  url=$(echo ${line} | cut -d , -f 3)
+  package_name=${arr[0]}
+  package_title=${arr[1]}
+  package_zh_name=${arr[2]}
+  url=${arr[3]}
   echo "update package name and url"
   # replace package info
+  $sd ${old_url} ${url} src-tauri/tauri.conf.json
+  $sd ${old_name} ${package_name} src-tauri/tauri.conf.json
+  echo "update ico with 32x32 pictue"
+  $sd ${old_name} ${package_name} src-tauri/src/main.rs
 
-  if [[ "$OSTYPE" =~ ^linux ]]; then
-    sed -i "s/${old_url}/${url}/g" src-tauri/tauri.conf.json
-    sed -i "s/${old_name}/${package_name}/g" src-tauri/tauri.conf.json
-    echo "update ico with 32x32 pictue"
-    sed -i "s/${old_name}/${package_name}/g" src-tauri/src/main.rs
-
-  fi
-
+  # for apple, need replace title
   if [[ "$OSTYPE" =~ ^darwin ]]; then
-    sed -i '' "s|${old_url}|${url}|g" src-tauri/tauri.conf.json
-    sed -i '' "s|${old_name}|${package_name}|g" src-tauri/tauri.conf.json
-    echo "update ico with 32x32 pictue"
-    sed -i '' "s|${old_name}|${package_name}|g" src-tauri/src/main.rs
+    $sd ${old_title} ${package_title} src-tauri/tauri.conf.json
   fi
   
   # echo "update ico with 32x32 pictue"
@@ -81,13 +82,14 @@ do
     old_desktop="src-tauri/assets/${package_prefix}-${old_name}.desktop"
     new_desktop="src-tauri/assets/${package_prefix}-${package_name}.desktop"
     mv ${old_desktop}  ${new_desktop}
-    sed -i "s/${old_zh_name}/${package_zh_name}/g" ${new_desktop}
-    sed -i "s/${old_name}/${package_name}/g" ${new_desktop}
+    $sd ${old_zh_name} ${package_zh_name} ${new_desktop}
+    $sd ${old_name} ${package_name} ${new_desktop}
   fi
 
   # update package info
-  old_zh_name=${package_zh_name}
   old_name=${package_name}
+  old_title=${package_title}
+  old_zh_name=${package_zh_name}
   old_url=${url}
 
   echo "building package ${index}/${total}"
@@ -104,7 +106,7 @@ do
     mv src-tauri/target/release/bundle/dmg/*.dmg output/macos
     echo ""
   fi
-done < app.csv
+done
 
 echo "build all package success!"
 if [[ "$OSTYPE" =~ ^linux ]]; then
