28cdd3627894113d5aec410ac42b47f25c2fc861
Tlntin <TlntinDeng01@Gmail.com>
2022-11-24 22:11:23 +0800

    更新Linux批量打包脚本

--- script/build.sh
+++ script/build.sh
@@ -112,11 +112,12 @@ done
 
 echo "build all package success!"
 if [[ "$OSTYPE" =~ ^linux ]]; then
+$sd "\"productName\": \"com-tw93-weread\"" "\"productName\": \"WeRead\"" src-tauri/tauri.conf.json
     echo "result file in output/linux"
 fi
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     # replace again
-    $sd "\"productName\": \"WeRead\"" "\"productName\": \"weread\"" src-tauri/tauri.conf.json
+    $sd "\"productName\": \"weread\"" "\"productName\": \"WeRead\"" src-tauri/tauri.conf.json
     echo "result file in output/macos"
 fi
