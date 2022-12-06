0d86d43db67853bfbe557cd683fabd655ecee27e
Tlntin <371043382@qq.com>
2022-12-06 18:22:46 +0800

    修复图标失效问题

--- bin/builders/common.ts
+++ bin/builders/common.ts
@@ -41,6 +41,28 @@ export async function mergeTauriConfig(
   tauriConf.package.productName = name;
   tauriConf.tauri.bundle.identifier = identifier;
   tauriConf.tauri.bundle.icon = [options.icon];
+  if (process.platform === "win32") {
+    const ico_path = path.join(npmDirectory, 'src-tauri/png/weread_32.ico');
+    await fs.copyFile(options.icon, ico_path);
+
+  }
+  let configPath = "";
+  switch (process.platform) {
+    case "win32": {
+      configPath = path.join(npmDirectory, 'src-tauri/tauri.windows.conf.json');
+      break;
+    }
+    case "darwin": {
+      configPath = path.join(npmDirectory, 'src-tauri/tauri.macos.conf.json');
+      break;
+    }
+  }
+  
+  let bundleConf = {tauri: {bundle: tauriConf.tauri.bundle}};
+  await fs.writeFile(
+    configPath,
+    Buffer.from(JSON.stringify(bundleConf), 'utf-8')
+  );
 
 
   const configJsonPath = path.join(npmDirectory, 'src-tauri/tauri.conf.json')
