d4aa9953d87239e7db8f077d2c13d7f46c7b7036
Tw93 <tw93@qq.com>
2023-03-10 21:59:35 +0800

    :sparkles: Support selection when downloading

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -26,7 +26,7 @@ use wry::application::window::Icon;
 #[cfg(any(target_os = "linux", target_os = "windows"))]
 use wry::webview::WebContext;
 
-use dirs::download_dir;
+use native_dialog::FileDialog;
 use std::path::PathBuf;
 
 enum UserEvent {
@@ -147,16 +147,18 @@ fn main() -> wry::Result<()> {
     let download_started = {
         let proxy = proxy.clone();
         move |uri: String, default_path: &mut PathBuf| {
-            let path = download_dir()
-                .unwrap()
-                .join(default_path.display().to_string())
-                .as_path()
-                .to_path_buf();
-            *default_path = path.clone();
-            let submitted = proxy
-                .send_event(UserEvent::DownloadStarted(uri, path.display().to_string()))
-                .is_ok();
-            submitted
+            let dialog_result = FileDialog::new()
+                .set_filename(&default_path.display().to_string())
+                .show_save_single_file();
+            if let Ok(Some(path)) = dialog_result {
+                *default_path = path.clone();
+                let submitted = proxy
+                    .send_event(UserEvent::DownloadStarted(uri, path.display().to_string()))
+                    .is_ok();
+                submitted
+            } else {
+                false
+            }
         }
     };
 
@@ -243,7 +245,7 @@ fn main() -> wry::Result<()> {
             Event::UserEvent(UserEvent::DownloadComplete(_, success)) => {
                 println!("Succeeded: {success}");
                 if success {
-                    let _ = webview.evaluate_script("window.pakeToast('Save in downloads folder')");
+                    let _ = webview.evaluate_script("window.pakeToast('Download Complete~')");
                 } else {
                     println!("No output path")
                 }
