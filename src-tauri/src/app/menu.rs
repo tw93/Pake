f36f492d2daa4c43e90d5c3622c26d0f064408da
Tw93 <tw93@qq.com>
2023-04-05 17:52:17 +0800

    :sparkles: 重构一下代码

--- src-tauri/src/app/menu.rs
+++ src-tauri/src/app/menu.rs
@@ -1,13 +1,6 @@
 #[cfg(target_os = "macos")]
 use tauri::MenuItem;
-
-use tauri::{
-    App, Config, CustomMenuItem, Menu, Submenu, TitleBarStyle, Window, WindowBuilder,
-    WindowMenuEvent, WindowUrl,
-};
-
-mod pake;
-use pake::PakeConfig;
+use tauri::{CustomMenuItem, Menu, Submenu, WindowMenuEvent};
 
 pub fn get_menu() -> Menu {
     // first menu
@@ -49,17 +42,6 @@ pub fn menu_event_handle(event: WindowMenuEvent) {
         "quit" => std::process::exit(0),
         _ => {}
     }
-}
-
-pub fn get_pake_config() -> (PakeConfig, Config) {
-    let pake_config_path = include_str!("../pake.json");
-    let pake_config: PakeConfig =
-        serde_json::from_str(pake_config_path).expect("failed to parse pake config");
-    // println!("{:#?}", config);
-    let tauri_config_path = include_str!("../tauri.conf.json");
-    let tauri_config: Config =
-        serde_json::from_str(tauri_config_path).expect("failed to parse tauri config");
-    (pake_config, tauri_config)
 }
 
 #[cfg(any(target_os = "linux", target_os = "windows"))]
@@ -126,68 +108,4 @@ pub fn system_tray_handle(app: &tauri::AppHandle, event: tauri::SystemTrayEvent)
             _ => {}
         }
     };
-}
-
-pub fn get_data_dir(_tauri_config: Config) -> std::path::PathBuf {
-    #[cfg(any(target_os = "linux", target_os = "windows"))]
-    let data_dir = {
-        let package_name = _tauri_config.package.product_name.unwrap();
-        let home_dir = match home::home_dir() {
-            Some(path1) => path1,
-            None => panic!("Error, can't found you home dir!!"),
-        };
-        #[cfg(target_os = "windows")]
-        let data_dir = home_dir.join("AppData").join("Roaming").join(package_name);
-        #[cfg(target_os = "linux")]
-        let data_dir = home_dir.join(".config").join(package_name);
-        if !data_dir.exists() {
-            std::fs::create_dir(&data_dir)
-                .unwrap_or_else(|_| panic!("can't create dir {}", data_dir.display()));
-        }
-        data_dir
-    };
-    #[cfg(target_os = "macos")]
-    let data_dir = std::path::PathBuf::new();
-    data_dir
-}
-
-pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: std::path::PathBuf) -> Window {
-    let window_config = config.windows.first().unwrap();
-    let user_agent = config.user_agent;
-    let url = match window_config.url_type.as_str() {
-        "web" =>WindowUrl::App(window_config.url.parse().unwrap()),
-        "local" => WindowUrl::App(std::path::PathBuf::from(&window_config.url)),
-        _ => panic!("url type only can be web or local"),
-    };
-    #[cfg(target_os = "macos")]
-    let window = WindowBuilder::new(app, "pake", url)
-        .title("")
-        .user_agent(user_agent.macos.as_str())
-        .resizable(window_config.resizable)
-        .fullscreen(window_config.fullscreen)
-        //用于隐藏头部
-        .title_bar_style(if window_config.transparent {
-            TitleBarStyle::Overlay
-        } else {
-            TitleBarStyle::Visible
-        })
-        .inner_size(window_config.width, window_config.height)
-        .initialization_script(include_str!("pake.js"));
-
-    #[cfg(any(target_os = "linux", target_os = "windows"))]
-    let window = {
-        #[cfg(target_os = "linux")]
-        let user_agent = user_agent.linux.as_str();
-        #[cfg(target_os = "windows")]
-        let user_agent = user_agent.windows.as_str();
-        WindowBuilder::new(app, "pake", url)
-            .title("")
-            .data_directory(_data_dir)
-            .resizable(window_config.resizable)
-            .fullscreen(window_config.fullscreen)
-            .user_agent(user_agent)
-            .inner_size(window_config.width, window_config.height)
-            .initialization_script(include_str!("pake.js"))
-    };
-    window.build().unwrap()
 }
---
+++ src-tauri/src/app/mod.rs
@@ -0,0 +1,4 @@
+pub mod menu;
+pub mod window;
+pub mod config;
+pub mod invoke;
