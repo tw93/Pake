20797ef6ecf4d42edca63a3ab850fc9917aa0a8e
Tw93 <tw93@qq.com>
2023-04-09 15:13:21 +0800

    :sparkles: Support opening with window.open

--- src-tauri/src/inject/event.js
+++ src-tauri/src/inject/event.js
@@ -141,8 +141,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Rewrite the window.open function.
   const originalWindowOpen = window.open;
   window.open = function (url, name, specs) {
-    console.log('window.open called with URL:', url);
-    // Call Rust code to open a browser.
     invoke('open_browser', { url });
 
     // Call the original window.open function to maintain its normal functionality.
