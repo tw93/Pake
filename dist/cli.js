3766cddc31d7f758cc6143c92f2f775680017611
Tw93 <tw93@qq.com>
2023-04-16 17:22:34 +0800

    :bug: Style compatibility

--- dist/cli.js
+++ dist/cli.js
@@ -2494,7 +2494,7 @@ class BuilderFactory {
 }
 
 var name = "pake-cli";
-var version = "2.0.0-alpha4";
+var version = "2.0.0-alpha5";
 var description = "🤱🏻 Turn any webpage into a desktop app with Rust. 🤱🏻 很简单的用 Rust 打包网页生成很小的桌面 App。";
 var engines = {
 	node: ">=16.0.0"
