02555088c4373fbc97b6d5562ad12dcd3b35a02c
Tw93 <tw93@qq.com>
2023-04-23 11:53:03 +0800

    :sparkles: fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -55,7 +55,6 @@ pub fn run_app() {
         })
         .on_window_event(|event| {
             if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
-
                 #[cfg(target_os = "macos")]
                 event.window().minimize().unwrap();
 
