050aff7cd60b5296155300cf938b1895031b75c3
Tw93 <tw93@qq.com>
2023-04-29 15:26:28 +0800

    :bookmark: 2.0.0

--- dist/cli.js
+++ dist/cli.js
@@ -2518,7 +2518,7 @@ class BuilderFactory {
 }
 
 var name = "pake-cli";
-var version = "2.0.0-alpha8";
+var version = "2.0.0";
 var description = "🤱🏻 Turn any webpage into a desktop app with Rust. 🤱🏻 很简单的用 Rust 打包网页生成很小的桌面 App。";
 var engines = {
 	node: ">=16.0.0"
