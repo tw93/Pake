f598a715221f2c247decd689ce5282ddda9cfa0c
Tw93 <tw93@qq.com>
2023-05-07 16:56:03 +0800

    :bookmark: add youtube music

--- dist/cli.js
+++ dist/cli.js
@@ -2523,7 +2523,7 @@ class BuilderFactory {
 }
 
 var name = "pake-cli";
-var version = "2.0.1";
+var version = "2.0.3";
 var description = "🤱🏻 Turn any webpage into a desktop app with Rust. 🤱🏻 很简单的用 Rust 打包网页生成很小的桌面 App。";
 var engines = {
 	node: ">=16.0.0"
