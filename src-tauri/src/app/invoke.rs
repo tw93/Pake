822c12fa19e557500640e1bd89df1e180ff47348
Tw93 <tw93@qq.com>
2023-06-07 14:51:06 +0800

    :sparkles: Code optimization

--- src-tauri/src/app/invoke.rs
+++ src-tauri/src/app/invoke.rs
@@ -9,26 +9,6 @@ pub struct DownloadFileParams {
 }
 
 #[command]
-pub fn drag_window(app: AppHandle) {
-    app.get_window("pake").unwrap().start_dragging().unwrap();
-}
-
-#[command]
-pub fn fullscreen(app: AppHandle) {
-    let win = app.get_window("pake").unwrap();
-    if win.is_fullscreen().unwrap() {
-        win.set_fullscreen(false).unwrap();
-    } else {
-        win.set_fullscreen(true).unwrap();
-    }
-}
-
-#[command]
-pub fn open_browser(app: AppHandle, url: String) {
-    api::shell::open(&app.shell_scope(), url, None).unwrap();
-}
-
-#[command]
 pub async fn download_file(app: AppHandle, params: DownloadFileParams) -> Result<(), String> {
     let window: Window = app.get_window("pake").unwrap();
     let output_path = api::path::download_dir().unwrap().join(params.filename);
