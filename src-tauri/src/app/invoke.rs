b26986e1422e7e7e378a9fee963ece9426af1562
jeasonnow <gosick.but@qq.com>
2023-06-25 15:30:11 +0800

    style: format code

--- src-tauri/src/app/invoke.rs
+++ src-tauri/src/app/invoke.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::fs;
 
 use crate::util::{check_file_or_append, get_download_message, show_toast};
 use download_rs::sync_download::Download;
@@ -34,18 +34,20 @@ pub async fn download_file(app: AppHandle, params: DownloadFileParams) -> Result
     }
 }
 
-
 #[command]
-pub async fn download_file_by_binary(app: AppHandle, params: BinaryDownloadParams) -> Result<(), String> {
+pub async fn download_file_by_binary(
+    app: AppHandle,
+    params: BinaryDownloadParams,
+) -> Result<(), String> {
     let window: Window = app.get_window("pake").unwrap();
     let output_path = api::path::download_dir().unwrap().join(params.filename);
     let file_path = check_file_or_append(output_path.to_str().unwrap());
-    let download_file_result = fs::write(&file_path, &params.binary);
+    let download_file_result = fs::write(file_path, &params.binary);
     match download_file_result {
         Ok(_) => {
             show_toast(&window, &get_download_message());
             Ok(())
-        },
+        }
         Err(e) => {
             show_toast(&window, &e.to_string());
             Err(e.to_string())
