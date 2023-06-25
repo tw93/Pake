f70920dbbebc70118c27c1f71647c4c0be54fa49
jeasonnow <gosick.but@qq.com>
2023-06-25 15:24:10 +0800

    feat: 调整下载全部通过 rs 能力实现

--- bin/utils/combine.ts
+++ bin/utils/combine.ts
@@ -6,7 +6,8 @@ export default async function combineFiles(files: string[], output: string) {
     if (file.endsWith('.css')) {
       return "window.addEventListener('DOMContentLoaded', (_event) => { const css = `" + fileContent + "`; const style = document.createElement('style'); style.innerHTML = css; document.head.appendChild(style); });";
     }
-    return fileContent;
+    
+    return "window.addEventListener('DOMContentLoaded', (_event) => { " + fileContent + " });";
   });
   fs.writeFileSync(output, contents.join('\n'));
   return files;
--- src-tauri/src/app/invoke.rs
+++ src-tauri/src/app/invoke.rs
@@ -1,3 +1,5 @@
+use std::{fs};
+
 use crate::util::{check_file_or_append, get_download_message, show_toast};
 use download_rs::sync_download::Download;
 use tauri::{api, command, AppHandle, Manager, Window};
@@ -6,6 +8,12 @@ use tauri::{api, command, AppHandle, Manager, Window};
 pub struct DownloadFileParams {
     url: String,
     filename: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct BinaryDownloadParams {
+    filename: String,
+    binary: Vec<u8>,
 }
 
 #[command]
@@ -19,6 +27,25 @@ pub async fn download_file(app: AppHandle, params: DownloadFileParams) -> Result
             show_toast(&window, &get_download_message());
             Ok(())
         }
+        Err(e) => {
+            show_toast(&window, &e.to_string());
+            Err(e.to_string())
+        }
+    }
+}
+
+
+#[command]
+pub async fn download_file_by_binary(app: AppHandle, params: BinaryDownloadParams) -> Result<(), String> {
+    let window: Window = app.get_window("pake").unwrap();
+    let output_path = api::path::download_dir().unwrap().join(params.filename);
+    let file_path = check_file_or_append(output_path.to_str().unwrap());
+    let download_file_result = fs::write(&file_path, &params.binary);
+    match download_file_result {
+        Ok(_) => {
+            show_toast(&window, &get_download_message());
+            Ok(())
+        },
         Err(e) => {
             show_toast(&window, &e.to_string());
             Err(e.to_string())
