6edc99f231856b2ac34632dcbbaacecc432b40b7
Tw93 <tw93@qq.com>
2024-04-15 20:55:53 +0800

    :bug: Compatible with new apps

--- src-tauri/src/inject/style.js
+++ src-tauri/src/inject/style.js
@@ -383,12 +383,12 @@ window.addEventListener('DOMContentLoaded', _event => {
       top: 28px;
     }
 
-    #pack-top-dom:active {
+    #pake-top-dom:active {
       cursor: grabbing;
       cursor: -webkit-grabbing;
     }
 
-    #pack-top-dom{
+    #pake-top-dom{
       position:fixed;
       background:transparent;
       top:0;
@@ -398,7 +398,7 @@ window.addEventListener('DOMContentLoaded', _event => {
       -webkit-app-region: drag;
       user-select: none;
       -webkit-user-select: none;
-      z-index: 90000;
+      z-index: 99999;
     }
 
     @media (max-width:767px){
