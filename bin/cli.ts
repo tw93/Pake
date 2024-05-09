2e95f4649879aefd86fe57c61c1022aec8477623
Tw93 <tw93@qq.com>
2024-05-09 19:46:27 +0800

    :art: Hide uncommon commands

--- bin/cli.ts
+++ bin/cli.ts
@@ -29,11 +29,9 @@ program
   .option('--icon <string>', 'Application icon', DEFAULT.icon)
   .option('--width <number>', 'Window width', validateNumberInput, DEFAULT.width)
   .option('--height <number>', 'Window height', validateNumberInput, DEFAULT.height)
-  .option('--transparent', 'Only for Mac, hide title bar', DEFAULT.transparent)
   .option('--fullscreen', 'Start in full screen', DEFAULT.fullscreen)
-  .option('--show-system-tray', 'Show system tray in app', DEFAULT.showSystemTray)
-  .option('--system-tray-icon <string>', 'Custom system tray icon', DEFAULT.systemTrayIcon)
-  .option('--iter-copy-file', 'Copy files when URL is a local file', DEFAULT.iterCopyFile)
+  .option('--hide-title-bar', 'Only for Mac, hide title bar', DEFAULT.transparent)
+  .option('--activation-shortcut <string>', 'Shortcut key to active App', DEFAULT.activationShortcut)
   .option('--multi-arch', 'Only for Mac, supports both Intel and M1', DEFAULT.multiArch)
   .option('--inject [injects...]', 'Injection of .js or .css Files', DEFAULT.inject)
   .option('--safe-domain [domains...]', 'Domains that Require Security Configuration"', DEFAULT.safeDomain)
@@ -42,6 +40,9 @@ program
   .addOption(new Option('--targets <string>', 'Only for Linux, option "deb" or "appimage"').default(DEFAULT.targets).hideHelp())
   .addOption(new Option('--always-on-top', 'Always on the top level').default(DEFAULT.alwaysOnTop).hideHelp())
   .addOption(new Option('--disabled-web-shortcuts', 'Disabled webPage shortcuts').default(DEFAULT.disabledWebShortcuts).hideHelp())
+  .addOption(new Option('--show-system-tray', 'Show system tray in app').default(DEFAULT.showSystemTray).hideHelp())
+  .addOption(new Option('--system-tray-icon <string>', 'Custom system tray icon').default(DEFAULT.systemTrayIcon).hideHelp())
+  .addOption(new Option('--transparent', 'Only for Mac, hide title bar').default(DEFAULT.transparent).hideHelp())
   .version(packageJson.version, '-v, --version', 'Output the current version')
   .action(async (url: string, options: PakeCliOptions) => {
     await checkUpdateTips();
