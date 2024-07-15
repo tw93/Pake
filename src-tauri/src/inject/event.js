ecfe2bf2800b81e55663eb7efdca3616f31e49c7
Tianj0o <2576099053@qq.com>
2024-07-15 19:52:19 +0800

    fix: window.open error caused by specs undefined

--- src-tauri/src/inject/event.js
+++ src-tauri/src/inject/event.js
@@ -236,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Apple login and google login
     if (name === 'AppleAuthentication') {
       //do nothing
-    } else if (specs.includes('height=') || specs.includes('width=')) {
+    } else if (specs && (specs.includes('height=') || specs.includes('width='))) {
       location.href = url;
     } else {
       const baseUrl = window.location.origin + window.location.pathname;
