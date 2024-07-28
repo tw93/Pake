8076b23d7c0c20360fc9fbd4c60e6095ddca11f1
m lau <279517546@qq.com>
2024-07-28 12:58:03 +0800

    Update defaults.ts
    Signed-off-by: m lau <279517546@qq.com>

--- bin/defaults.ts
+++ bin/defaults.ts
@@ -1,7 +1,7 @@
 import { PakeCliOptions } from './types.js';
 
 export const DEFAULT_PAKE_OPTIONS: PakeCliOptions = {
-  icon: '',
+  icon: 'https://ootheme.com/mytv/favicon.ico',
   height: 780,
   width: 1200,
   fullscreen: false,
@@ -24,8 +24,8 @@ export const DEFAULT_PAKE_OPTIONS: PakeCliOptions = {
 // Just for cli development
 export const DEFAULT_DEV_PAKE_OPTIONS: PakeCliOptions & { url: string } = {
   ...DEFAULT_PAKE_OPTIONS,
-  url: 'https://weread.qq.com',
-  name: 'WeRead',
-  safeDomain: ['weread.qq.com'],
+  url: 'https://ootheme.com/mytv',
+  name: 'NyTv',
+  safeDomain: ['ootheme.com'],
   hideTitleBar: true,
 };
