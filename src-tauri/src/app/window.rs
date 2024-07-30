8c07aaae3c4cec2acf02fedcbcb7fcbfb5edd286
jeasonnow <gosick.but@qq.com>
2024-07-30 10:53:06 +0800

    refactor: 升级 tauri 到 2.x

--- src-tauri/src/app/window.rs
+++ src-tauri/src/app/window.rs
@@ -1,11 +1,11 @@
 use crate::app::config::PakeConfig;
 use std::path::PathBuf;
-use tauri::{App, Window, WindowBuilder, WindowUrl};
+use tauri::{App, WebviewUrl, WebviewWindow, WebviewWindowBuilder};
 
 #[cfg(target_os = "macos")]
 use tauri::TitleBarStyle;
 
-pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: PathBuf) -> Window {
+pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: PathBuf) -> WebviewWindow {
     let window_config = config
         .windows
         .first()
@@ -14,8 +14,8 @@ pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: PathBuf) -> Wind
     let user_agent = config.user_agent.get();
 
     let url = match window_config.url_type.as_str() {
-        "web" => WindowUrl::App(window_config.url.parse().unwrap()),
-        "local" => WindowUrl::App(PathBuf::from(&window_config.url)),
+        "web" => WebviewUrl::App(window_config.url.parse().unwrap()),
+        "local" => WebviewUrl::App(PathBuf::from(&window_config.url)),
         _ => panic!("url type can only be web or local"),
     };
 
@@ -24,14 +24,14 @@ pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: PathBuf) -> Wind
         serde_json::to_string(&window_config).unwrap()
     );
 
-    let mut window_builder = WindowBuilder::new(app, "pake", url)
+    let mut window_builder = WebviewWindowBuilder::new(app, "pake", url)
         .title("")
         .user_agent(user_agent)
         .visible(false) // Prevent initial shaking
         .resizable(window_config.resizable)
         .fullscreen(window_config.fullscreen)
         .inner_size(window_config.width, window_config.height)
-        .disable_file_drop_handler()
+        .disable_drag_drop_handler()
         .always_on_top(window_config.always_on_top)
         .initialization_script(&config_script)
         .initialization_script(include_str!("../inject/component.js"))
---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,90 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+mod app;
+mod util;
+
+use std::str::FromStr;
+
+use app::{invoke, menu::set_system_tray, window};
+use invoke::{download_file, download_file_by_binary};
+use tauri::Manager;
+use tauri_plugin_window_state::Builder as windowStatePlugin;
+use tauri_plugin_global_shortcut::Shortcut;
+use util::{get_data_dir, get_pake_config};
+use window::get_window;
+
+pub fn run_app() {
+    let (pake_config, tauri_config) = get_pake_config();
+
+    let mut tauri_app = tauri::Builder::default();
+
+    let show_system_tray = pake_config.show_system_tray();
+
+    // Save the value of toggle_app_shortcut before pake_config is moved
+    let activation_shortcut = pake_config.windows[0].activation_shortcut.clone();
+
+    tauri_app
+        .plugin(windowStatePlugin::default().build())
+        .plugin(tauri_plugin_oauth::init())
+        .plugin(tauri_plugin_http::init())
+        .invoke_handler(tauri::generate_handler![
+            download_file,
+            download_file_by_binary
+        ])
+        .setup(move |app| {
+            let data_dir = get_data_dir(&app.app_handle(), tauri_config);
+
+            let _window = get_window(app, pake_config, data_dir);
+            // Prevent initial shaking
+            _window.show().unwrap();
+
+            if show_system_tray {
+                let _ = set_system_tray(&app.app_handle());
+            }
+
+            if !activation_shortcut.is_empty() {
+                let app_handle = app.app_handle().clone();
+                app_handle
+                    .plugin(
+                        tauri_plugin_global_shortcut::Builder::new()
+                            .with_shortcut(activation_shortcut.as_str())
+                            ?.with_handler(move |app, event, _shortcut| {
+                                let shortcut_hotkey = Shortcut::from_str(&activation_shortcut.as_str()).unwrap();
+
+                                if shortcut_hotkey.eq(event) {
+                                    let window = app.get_webview_window("pake").unwrap();
+                                    match window.is_visible().unwrap() {
+                                        true => window.hide().unwrap(),
+                                        false => {
+                                            window.show().unwrap();
+                                            window.set_focus().unwrap();
+                                        }
+                                    }
+                                }
+                            }
+                    ).build())
+                    .expect("Error registering global evoke shortcuts!");
+            }
+
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                #[cfg(target_os = "macos")]
+                {
+                    window.minimize().unwrap();
+                    window.hide().unwrap();
+                }
+
+                #[cfg(not(target_os = "macos"))]
+                event.window().close().unwrap();
+
+                api.prevent_close();
+            }
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+pub fn run() {
+    run_app()
+}
