4458a7b8cf52f5518580899b6c293a4844a9d122
jeasonnow <gosick.but@qq.com>
2024-08-05 12:44:46 +0800

    fix: fix linux/windows title

--- src-tauri/src/app/window.rs
+++ src-tauri/src/app/window.rs
@@ -54,7 +54,7 @@ pub fn get_window(app: &mut App, config: PakeConfig, _data_dir: PathBuf) -> Wind
     {
         window_builder = window_builder
             .data_directory(_data_dir)
-            .title(app.package_info().name)
+            .title(app.package_info().name.clone())
     }
 
     window_builder.build().expect("Failed to build window")
