e344cd390436dcf22eb0a284400eb2063cce8b2b
木伦尺八 <81153379+Albatross7@users.noreply.github.com>
2025-06-09 17:15:04 +0800

    Update build_with_pake_cli.js
    add --show-menu
    
    Signed-off-by: 木伦尺八 <81153379+Albatross7@users.noreply.github.com>

--- script/build_with_pake_cli.js
+++ script/build_with_pake_cli.js
@@ -20,7 +20,7 @@ console.log('targets type? only for Linux: ', process.env.TARGETS);
 console.log('===========================\n');
 
 cd('node_modules/pake-cli');
-let params = `node cli.js ${process.env.URL} --name ${process.env.NAME} --height ${process.env.HEIGHT} --width ${process.env.WIDTH}`;
+let params = `node cli.js ${process.env.URL} --name ${process.env.NAME} --height ${process.env.HEIGHT} --width ${process.env.WIDTH}` --show-menu;
 
 if (process.env.HIDE_TITLE_BAR === 'true') {
   params = `${params} --hide-title-bar`;
