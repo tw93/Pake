df0056accebb67d5e08d5fcf189d03c3c82061a9
Tw93 <tw93@qq.com>
2025-12-08 13:46:23 +0800

    Fix possible grammatical errors injected

--- bin/utils/combine.ts
+++ bin/utils/combine.ts
@@ -2,15 +2,17 @@ import fs from 'fs';
 
 export default async function combineFiles(files: string[], output: string) {
   const contents = files.map((file) => {
-    const fileContent = fs.readFileSync(file);
     if (file.endsWith('.css')) {
-      return (
-        "window.addEventListener('DOMContentLoaded', (_event) => { const css = `" +
-        fileContent +
-        "`; const style = document.createElement('style'); style.innerHTML = css; document.head.appendChild(style); });"
-      );
+      const fileContent = fs.readFileSync(file, 'utf-8');
+      return `window.addEventListener('DOMContentLoaded', (_event) => {
+        const css = ${JSON.stringify(fileContent)};
+        const style = document.createElement('style');
+        style.innerHTML = css;
+        document.head.appendChild(style);
+      });`;
     }
 
+    const fileContent = fs.readFileSync(file);
     return (
       "window.addEventListener('DOMContentLoaded', (_event) => { " +
       fileContent +
