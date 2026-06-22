84f164637402f42bc3dba1700a71383207b5397b
GitHub Action <action@github.com>
2026-06-22 00:46:46 +0000

    Auto-fix formatting issues

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,8 +19,8 @@ const WEBKIT_DISABLE_COMPOSITING_MODE: &str = "WEBKIT_DISABLE_COMPOSITING_MODE";
 
 use app::{
     invoke::{
-        clear_dock_badge, download_file, increment_dock_badge, send_notification,
-        set_dock_badge, set_dock_badge_label, update_theme_mode,
+        clear_dock_badge, download_file, increment_dock_badge, send_notification, set_dock_badge,
+        set_dock_badge_label, update_theme_mode,
     },
     setup::{set_global_shortcut, set_system_tray},
     window::{open_additional_window_safe, set_window, MultiWindowState},
