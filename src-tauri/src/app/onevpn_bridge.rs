c2db1a1224f355861e8fe029e2d624943cfd1f80
Jingtu Li <264193564+SupaSuper@users.noreply.github.com>
2026-06-23 18:25:01 -0500

    Remove unsupported bridge placeholder

--- src-tauri/src/app/onevpn_bridge.rs
+++
@@ -1,9 +0,0 @@
-use crate::app::config::OneVpnConfig;
-use std::io;
-
-pub fn start_onevpn_bridge(_config: &OneVpnConfig) -> io::Result<String> {
-    Err(io::Error::new(
-        io::ErrorKind::Unsupported,
-        "upstream bridge implementation is not enabled in this build",
-    ))
-}
