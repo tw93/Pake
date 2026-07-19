b979e9fd1dc6a59db7051cb4cc40ea3d8f47223c
Tw93 <hitw93@gmail.com>
2026-07-19 21:30:06 +0800

    chore: bump version to V3.15.2

--- dist/cli.js
+++ dist/cli.js
@@ -20,7 +20,7 @@ import * as psl from 'psl';
 import { InvalidArgumentError, program as program$1, Option } from 'commander';
 
 var name = "pake-cli";
-var version = "3.15.1";
+var version = "3.15.2";
 var description = "🤱🏻 Turn any webpage into a desktop app with one command. 🤱🏻 一键打包网页生成轻量桌面应用。";
 var engines = {
 	node: ">=18.0.0"
